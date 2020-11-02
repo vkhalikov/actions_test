474c7786686ae84ceb3c30f252899fbf6ab172ba
Vadim Khalikov <vkhalikov.work@gmail.com>
2020-11-02 17:45:49 +0300

    reworked comment creation

--- .github/actions/create-service-comment/create-service-comment.js
+++ .github/actions/create-service-comment/create-service-comment.js
@@ -1,28 +1,58 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function run() {
+const ACTION_NAME = 'Create Service Comment';
+const DEFAULT_INFO_MESSAGE = `###### This comment was generated automatically by ${ACTION_NAME} action\n---\n`;
+
+const constructCommentBody = (placeholders, { bold = true, infoMessage = DEFAULT_INFO_MESSAGE } = {}) => {
+  const getBaseMessage = () => infoMessage !== 'false' ? infoMessage : '';
+
+  if (!placeholders) {
+    return getBaseMessage();
+  }
+
+  if (!Array.isArray(placeholders)) {
+    throw new TypeError(`Placeholders should be an array, got ${typeof placeholders} instead`);
+  }
+
+  let finalPlaceholders = placeholders;
+
+  if (bold) {
+    finalPlaceholders = placeholders.map((placeholder) => `**${placeholder}**`);
+  }
+
+  const placeholdersString = finalPlaceholders.join(':\n');
+
+  return getBaseMessage() + placeholdersString;
+};
+
+const run = async () => {
   try {
     const authToken = core.getInput('auth-token');
-    const octokit = github.getOctokit(authToken, { userAgent: 'create-service-comment-action' });
-
+    const octokit = github.getOctokit(authToken, { userAgent: ACTION_NAME });
     const context = github.context;
 
     console.log(`TOKEN: ${authToken}`);
-    console.log(`The context: ${JSON.stringify(context, null, 2)}`);
-    console.log(`The repo: ${JSON.stringify(context.repo, null, 2)}`);
 
-    // const { data: pullRequest } = await octokit.pulls.get({
-    //   owner: 'octokit',
-    //   repo: 'rest.js',
-    //   pull_number: 123,
-    //   mediaType: {
-    //     format: 'diff'
-    //   }
-    // });
+    const placeholders = core.getInput('placeholders');
+    const commentConstructorOptions = {
+      bold: Boolean(core.getInput('bold')),
+      infoMessage: core.getInput('info-message'),
+    };
+
+    console.log(commentConstructorOptions);
+
+    const body = constructCommentBody(placeholders, commentConstructorOptions);
+
+    const comment = await octokit.issues.createComment({
+      body,
+      ...context.issue,
+    });
+
+    console.log(comment);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
 run();
